285cc24038ad5747ef34b96ff2417ab417c6da8a
Bradley Farias <bfarias@godaddy.com>
2018-03-30 20:35:24 -0500

    live streamed this at https://www.youtube.com/watch?v=OmdzxuwGE50

---
+++ node.js
@@ -0,0 +1,8 @@
+console.log('node started');
+debugger;
+console.log('loop starting');
+let i = 0;
+while (true) {
+  console.log('loop', i++);
+  debugger;
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+References
+
+* https://chromedevtools.github.io/devtools-protocol/v8/
+* https://github.com/buggerjs/bugger-v8-client/blob/master/PROTOCOL.md
